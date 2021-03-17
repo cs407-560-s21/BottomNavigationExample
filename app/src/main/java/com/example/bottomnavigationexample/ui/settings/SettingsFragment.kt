b30f34e9eaed36ae4c50390c78d92ec41e582c5d
CS416F19 <cs416f19@gmail.com>
2021-03-17 13:35:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigationexample/ui/settings/SettingsFragment.kt
@@ -0,0 +1,33 @@
+package com.example.bottomnavigationexample.ui.settings
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import com.example.bottomnavigationexample.R
+
+class SettingsFragment : Fragment() {
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        settingsViewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
+        val textView: TextView = root.findViewById(R.id.text_settings)
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+
+    }
+
+
+}
